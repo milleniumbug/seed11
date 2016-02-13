81d005891673294182fdb65e81416688e2e17ac0
milleniumbug <wojas1222@gmail.com>
2016-02-13 03:34:32 +0100

    fixed the test

--- tests/multiple_runs_over_second.sh
+++ tests/multiple_runs_over_second.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for i in `seq 1 10`; do
-	./build/random_output ascii_newlines seed_device > "tempout$i"
+	./build/random_output raw seed_device 256 > "tempout$i"
 done
 
 for i in `seq 2 10`; do
-	if cmp "tempout1" "tempout$i"; then
+	if cmp "tempout1" "tempout$i" -s; then
 		echo "FAIL"
 	fi
 done
