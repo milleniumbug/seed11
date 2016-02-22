05fe60997ce108d9ef2bc7b8b37437ff5e8d14f6
milleniumbug <wojas1222@gmail.com>
2016-02-22 04:00:13 +0100

    moved seed_device to separate header
    the backward compatibility is satisfied since `<seed11/seed11.hpp>` includes `<seed11/seed_device.hpp>`

--- include/seed11/seed11.hpp
+++ include/seed11/seed11.hpp
@@ -12,55 +12,10 @@
 #include <type_traits>
 #include <algorithm>
 
+#include <seed11/seed_device.hpp>
+
 namespace seed11
 {
-	class seed_device_init_error : public std::runtime_error
-	{
-	public:
-		explicit seed_device_init_error(const std::string& what_arg);
-		explicit seed_device_init_error(const char* what_arg);
-	};
-
-	class seed_device_read_error : public std::runtime_error
-	{
-	public:
-		explicit seed_device_read_error(const std::string& what_arg);
-		explicit seed_device_read_error(const char* what_arg);
-	};
-
-	class seed_device
-	{
-		struct seed_impl_deleter
-		{
-			void operator()(void* ptr);
-		};
-		std::unique_ptr<void, seed_impl_deleter> impl;
-	public:
-		typedef unsigned result_type;
-
-		static constexpr result_type min()
-		{
-			return 0;
-		}
-
-		static constexpr result_type max()
-		{
-			return UINT_MAX;
-		}
-
-		double entropy() const;
-
-		result_type operator()();
-
-		seed_device();
-		explicit seed_device(const std::string& token);
-		seed_device(const seed_device&) = delete;
-		seed_device(seed_device&&) = delete;
-		seed_device& operator=(const seed_device&) = delete;
-		seed_device& operator=(seed_device&&) = delete;
-		~seed_device() = default;
-	};
-
 	template<typename T>
 	struct seed_size
 	{
